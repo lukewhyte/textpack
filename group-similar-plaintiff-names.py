44db3da1c19a9ee686308291b7520e42565805e8
Luke Whyte <lukeawhyte@gmail.com>
2019-06-26 15:16:59 -0500

    Switched to awesome_cossim_topn

--- group-similar-plaintiff-names.py
+++ group-similar-plaintiff-names.py
@@ -2,12 +2,11 @@ import re
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy.sparse import csr_matrix
+from sparse_dot_topn import awesome_cossim_topn
 
 
 class ColumnValueGrouper():
-    def __init__(self, csv_path, column_to_group, export_path, match_threshold=0.8, ngram_length=3):
+    def __init__(self, csv_path, column_to_group, export_path, match_threshold=0.75, ngram_length=3):
         self._df = pd.read_csv(csv_path)
         self._column = column_to_group
         self._match_threshold = match_threshold
@@ -20,16 +19,13 @@ class ColumnValueGrouper():
         ngrams = zip(*[string[i:] for i in range(self._ngram_length)])
         return [''.join(ngram) for ngram in ngrams]
 
-    def _get_tf_idf_matrix(self):
+    def _get_tf_idf_matrix(self, vals):
         vectorizer = TfidfVectorizer(analyzer=self._ngrams_analyzer)
-        return vectorizer.fit_transform(self._df[self._column].unique())
+        return vectorizer.fit_transform(vals)
 
-    def _get_cosine_matrix(self):
-        tf_idf_matrix = self._get_tf_idf_matrix()
-        return cosine_similarity(tf_idf_matrix, dense_output=False)
-
-    def _vals_are_siblings(self, y, x, prox):
-        return (x != y) and (prox > self._match_threshold)
+    def _get_cosine_matrix(self, vals):
+        tf_idf_matrix = self._get_tf_idf_matrix(vals)
+        return awesome_cossim_topn(tf_idf_matrix, tf_idf_matrix.transpose(), vals.size, self._match_threshold)
 
     def _find_group(self, y, x):
         if y in self._group_lookup:
@@ -43,22 +39,23 @@ class ColumnValueGrouper():
         self._group_lookup[y] = group
         self._group_lookup[x] = group
 
-    def _add_pair_to_lookup(self, row, col, data):
-        if self._vals_are_siblings(row, col, data):
-            group = self._find_group(row, col)
-            if group is not None:
-                self._add_vals_to_lookup(group, row, col)
-            else:
-                self._add_vals_to_lookup(row, row, col)
+    def _add_pair_to_lookup(self, row, col):
+        group = self._find_group(row, col)
+        if group is not None:
+            self._add_vals_to_lookup(group, row, col)
+        else:
+            self._add_vals_to_lookup(row, row, col)
 
     def build_group_lookup(self):
-        print('Building the TF-IDF, Cosine & Coord matrices...')
-        coord_matrix = self._get_cosine_matrix().tocoo()
-
-        print('Building the group lookup. This could take a little while...')
         vals = self._df[self._column].unique()
-        for row, col, data in zip(coord_matrix.row, coord_matrix.col, coord_matrix.data):
-            self._add_pair_to_lookup(vals[row], vals[col], data)
+
+        print('Building the TF-IDF, Cosine & Coord matrices...')
+        coord_matrix = self._get_cosine_matrix(vals).tocoo()
+
+        print('Building the group lookup...')
+        for row, col in zip(coord_matrix.row, coord_matrix.col):
+            if row != col:
+                self._add_pair_to_lookup(vals[row], vals[col])
 
     def add_grouped_column_to_data(self, column_name='Group'):
         print('Adding grouped columns to data frame...')
